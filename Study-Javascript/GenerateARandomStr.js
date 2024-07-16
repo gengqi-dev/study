04c1095f19fcd2bf714b3ee359a82dad07d2f00a
Charles Geng <geng-q@x1studio.co.jp>
2024-07-16 16:17:32 +0900

    Add professional portfolio

--- Study-Javascript/GenerateARandomStr.js
+++ Study-Javascript/GenerateARandomStr.js
@@ -1,2 +1,7 @@
-let randStr = Math.random().toString(36).slice(2); // "bnh5ytm"
-console.log(randStr);
+let randStr1 = Math.random().toString(36).slice(2); // "bnh5ytm"
+let randStr2 = Math.random().toString(36).slice(2); // "bnh5ytm"
+console.log(randStr1 + randStr2);
+let rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+for (const row of rows) {
+    console.log('*'.repeat(row));
+}
